37f3b7274c1ae160898cf878564cc58ea8177e70
Bognar Junior <bognar_junior@yahoo.com.br>
2022-07-26 21:58:52 -0300

    Create image top
    Create image top

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StatusBar, SafeAreaView } from 'react-native';
 import Basket from './src/screens/Basket';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar />
       <Basket />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- orgs-cesta/src/screens/Basket.js
+++ orgs-cesta/src/screens/Basket.js
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
+import { Text, Image, StyleSheet, Dimensions } from 'react-native'
 import React from 'react'
+import imageTop from './../../assets/topo.png'
+
+const width = Dimensions.get("screen").width;
 
 export default function Basket() {
   return (
-    <View>
-      <Text>Basket</Text>
-    </View>
+    <>
+      <Image source={imageTop} style={style.top}/>
+      <Text style={style.title}>Detalhe da Cesta</Text>
+    </>
   )
 }
+
+const style = StyleSheet.create({
+  top: {
+    width: "100%",
+    height: 578 / 768 * width,
+  },
+  title: {
+    top: 44,
+    width: "100%",
+    position: 'absolute',
+    textAlign: 'center',
+    fontSize: 16,
+    lineHeight: 26,
+    color: "white",
+    fontWeight: "bold",
+    padding: 16,
+  }
+}) 
