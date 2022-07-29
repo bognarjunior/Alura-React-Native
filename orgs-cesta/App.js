c7d575ba4f4378c0a3de6f720d39316ca8707295
Bognar Junior <bognar_junior@yahoo.com.br>
2022-07-28 21:19:28 -0300

    Customizing a font
    Customizing a font

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+
 import Basket from './src/screens/Basket';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <View />;
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
--- orgs-cesta/src/screens/Basket.js
+++ orgs-cesta/src/screens/Basket.js
@@ -1,6 +1,7 @@
-import { Text, Image, StyleSheet, Dimensions } from 'react-native'
+import { Text, Image, StyleSheet, Dimensions, View } from 'react-native'
 import React from 'react'
 import imageTop from './../../assets/topo.png'
+import logo from './../../assets/logo.png'
 
 const width = Dimensions.get("screen").width;
 
@@ -9,6 +10,15 @@ export default function Basket() {
     <>
       <Image source={imageTop} style={style.top}/>
       <Text style={style.title}>Detalhe da Cesta</Text>
+      <View style={style.wrapper}>
+        <Text style={style.name}>Cesta de verduras</Text>
+        <View style={style.wrapperFarm}>
+          <Image source={logo} style={style.imageFarm}/>
+          <Text style={style.nameFarm}>Jenny Jack Farm</Text>
+        </View>
+        <Text style={style.description}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para a sua cozinha.</Text>
+        <Text style={style.price}>R$ 40,00</Text>
+      </View>
     </>
   )
 }
@@ -28,5 +38,41 @@ const style = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     padding: 16,
+  },
+  wrapper: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  name: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: "#464646",
+    fontFamily: "MontserratBold",
+  },
+  wrapperFarm: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+  imageFarm: {
+    width: 32,
+    height: 32,
+  },
+  nameFarm: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+    fontFamily: "MontserratRegular",
+  },
+  description: {
+    color: "#A3A3A3",
+    fontSize: 16,
+    lineHeight: 26
+  },
+  price: {
+    color: "#2A9F85",
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: "bold",
+    marginTop: 8,
   }
 }) 
