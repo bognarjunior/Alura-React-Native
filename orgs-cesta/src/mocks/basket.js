a361a11899fe6d2771f086cd3bcc38882516274d
Bognar Junior <bognar_junior@yahoo.com.br>
2022-07-30 21:25:32 -0300

    Update Mock
    Update mock

--- orgs-cesta/src/mocks/basket.js
+++ orgs-cesta/src/mocks/basket.js
@@ -1,5 +1,11 @@
 
 import logo from './../../assets/logo.png'
+import tomato from '../../assets/frutas/Tomate.png';
+import broccoli from '../../assets/frutas/Brócolis.png';
+import potato from '../../assets/frutas/Batata.png';
+import cucumber from '../../assets/frutas/Pepino.png';
+import pumpkin from '../../assets/frutas/Abóbora.png';
+
 const basket = {
   top:  {
     title: "Detalhe da Cesta",
@@ -10,6 +16,31 @@ const basket = {
     nameFarm: "Jenny Jack Farm",
     description: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para a sua cozinha.",
     price: "R$ 40,00",
+  },
+  items: {
+    title: "Itens da cesta",
+    list: [
+      {
+        name: "Tomate",
+        image: tomato,
+      },
+      {
+        name: "Brócolis",
+        image: broccoli,
+      },
+      {
+        name: "Batata",
+        image: potato,
+      },
+      {
+        name: "Pepino",
+        image: cucumber,
+      },
+      {
+        name: "Abóbora",
+        image: pumpkin,
+      }
+    ]
   }
 }
 
