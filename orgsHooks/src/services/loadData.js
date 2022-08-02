c77148b6410a8ffa4766a66261d1fbecc6946185
Bognar Junior <bognar_junior@yahoo.com.br>
2022-08-01 22:42:58 -0300

    Create loadData
    Create loadData

---
+++ orgsHooks/src/services/loadData.js
@@ -0,0 +1,10 @@
+import producers from '../mocks/producers';
+import top from '../mocks/top';
+
+export const getProducers = () => {
+  return producers;
+};
+
+export const getTop = () => {
+  return top;
+};
