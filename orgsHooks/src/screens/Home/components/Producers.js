f4c71a8e9f8a431189415a6b4506faec45c886a9
Bognar Junior <bognar_junior@yahoo.com.br>
2022-08-03 22:06:18 -0300

    Create producers
    Create producers component and import into index

---
+++ orgsHooks/src/screens/Home/components/Producers.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function Producers() {
+  return (
+    <View>
+      <Text>Producers</Text>
+    </View>
+  );
+}
--- orgsHooks/src/screens/Home/index.js
+++ orgsHooks/src/screens/Home/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Producers from './components/Producers';
 import Top from './components/Top';
 
 const Home = () => {
-  return <Top />;
+  return (
+    <>
+      <Top />;
+      <Producers />
+    </>
+  );
 };
 
 export default Home;
