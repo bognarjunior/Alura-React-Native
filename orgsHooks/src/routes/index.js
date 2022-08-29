298d433c297529aee92df432fa2cc3282e097e82
Bognar Junior <bognar_junior@yahoo.com.br>
2022-08-29 14:26:29 -0300

    Add Icons and update Tab
    Add Icons and update Tab

--- orgsHooks/src/routes/index.js
+++ orgsHooks/src/routes/index.js
@@ -4,14 +4,29 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ProducersRoutes from './ProducersRoutes';
 import BestProducersRoutes from './BestPrducerRoutes';
 
+import Heart from '../assets/heart.svg';
+import Home from '../assets/home.svg';
+
 const Tab = createBottomTabNavigator();
 const Routes = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={{
+        screenOptions={({route}) => ({
           headerShown: false,
-        }}>
+          tabBarLabel: route.name === 'Home' ? 'Home' : 'Melhores Produtores',
+          tabBarIcon: ({color}) => {
+            const {name} = route;
+            let Icon = Home;
+
+            if (name === 'BestProducers') {
+              Icon = Heart;
+            }
+            return <Icon color={color} />;
+          },
+          tabBarActiveTintColor: '#2A9F85',
+          tabBarInactiveTintColor: '#C7C7C7',
+        })}>
         <Tab.Screen name="Home" component={ProducersRoutes} />
         <Tab.Screen name="BestProducers" component={BestProducersRoutes} />
       </Tab.Navigator>
